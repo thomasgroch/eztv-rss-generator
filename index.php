1f33025f8630523e8bd9d724cfe3136d42364630
Thomas Letsch Groch <thomas.groch@gmail.com>
2016-10-05 02:33:47 -0300

    Quick update to keep working and auto sort desc by name

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 
 include("simple_html_dom.php");
 
-$html = file_get_html('http://eztv.it/shows/'.$_REQUEST['show']);
+$html = file_get_html('http://eztv.ag/shows/'.$_REQUEST['show']);
 
-//header("content-type:text/xml");
+header("content-type:text/xml");
 
 echo '<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE torrent PUBLIC "-//bitTorrent//DTD torrent 0.1//EN" "http://xmlns.ezrss.it/0.1/dtd/">
@@ -13,13 +13,16 @@ echo '<?xml version="1.0" encoding="UTF-8"?>
 		<title>ezRSS - Search Results</title>
 		<description>Custom RSS feed based off search filters.</description>';
 
-foreach($html->find('a') as $element) {
-	if($element->class == 'magnet'){//download_1
-		echo "<item>";
-		echo '<title><![CDATA['.$element->parent()->prev_sibling()->first_child()->title.']]></title>
-			<link><![CDATA['.$element->href.']]></link>';
-		echo "</item>";
-	}
+foreach($html->find('a[class=magnet]') as $element) {
+	$links[$element->parent()->prev_sibling()->first_child()->title] = $element->href;
+}
+
+arsort($links);
+foreach($links as $key => $link) {
+	echo "<item>";
+	echo '<title><![CDATA['.$key.']]></title>
+		<link><![CDATA['.$link.']]></link>';
+	echo "</item>";
 }
 
 echo '</channel>
